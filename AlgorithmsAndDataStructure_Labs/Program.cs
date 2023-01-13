ae6ae29845f67e28fc4005658fd93b89d5b4b9e0
Gabriel <gnicholauson@gmail.com>
2023-01-13 15:46:28 -0600

    add header

--- AlgorithmsAndDataStructure_Labs/Program.cs
+++ AlgorithmsAndDataStructure_Labs/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Lab 3 
+ * Gabriel Nicholauson
+ * 
+ * Complexity
+ * This program will:
+ * - Find all duplicate elements in an array
+ * - Combine two arrays and sort them
+ * - reverse the digits of an integer
+ * 
+*/
+
