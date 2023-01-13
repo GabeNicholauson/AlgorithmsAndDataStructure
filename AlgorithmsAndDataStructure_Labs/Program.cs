67ab855b2354292190e980d65393a3a4bbca3cb5
Gabriel <gnicholauson@gmail.com>
2023-01-12 20:39:38 -0600

    add reverse string function

--- AlgorithmsAndDataStructure_Labs/Program.cs
+++ AlgorithmsAndDataStructure_Labs/Program.cs
@@ -5,3 +5,17 @@
  * This program will take a string, return all the letters that appear more than once, return 
  * all unique words, reverse the string and return the longest word.
 */
+
+using System.Text;
+
+string example = "Today I will eat cereal, watch a movie, and then shovel my driveway.";
+
+/**** Reverse String ****/
+
+StringBuilder sb = new StringBuilder();
+
+for (int i = example.Length - 1; i >= 0; i--)
+{
+    sb.Append(example[i]);
+}
+Console.WriteLine(sb.ToString());
