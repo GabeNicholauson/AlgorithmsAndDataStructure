3e355bfc82811f50a9967bd8b71b645eb4b8de53
Gabriel <gnicholauson@gmail.com>
2023-01-12 20:35:52 -0600

    Add header

--- AlgorithmsAndDataStructure_Labs/Program.cs
+++ AlgorithmsAndDataStructure_Labs/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿/*
+ * Lab 2
+ * Gabriel Nicholauson
+ * 
+ * This program will take a string, return all the letters that appear more than once, return 
+ * all unique words, reverse the string and return the longest word.
+*/
