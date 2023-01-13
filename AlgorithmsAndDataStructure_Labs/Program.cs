0f9a09f7a015a57a248d9cc644415e78a1031979
Gabriel <gnicholauson@gmail.com>
2023-01-13 16:41:49 -0600

    add ability to merge and sort arrays

--- AlgorithmsAndDataStructure_Labs/Program.cs
+++ AlgorithmsAndDataStructure_Labs/Program.cs
@@ -17,7 +17,7 @@ int[] ints = new int[] { 1, 2, 3, 4, 7, 9, 2, 4 };
 string trackedNums = "";
 string recurringNums = "";
 
-foreach(int num in ints)
+foreach (int num in ints)
 {
     if (!trackedNums.Contains(num.ToString()))
     {
@@ -34,6 +34,13 @@ Console.WriteLine(recurringNums);
 /**** Combine arrays and sort them ****/
 /**************************************/
 
+// It is not possible to merge the arrays and keep them sorted in O(n) time.
+// Combining the arrays and sorting them are both O(n) operations.
+int[] array1 = { 1, 2, 3, 4, 5, 5, 6, 7, 5, 6, 9, 4, 2 };
+int[] array2 = { 2, 5, 7, 9, 13, 9, 9, 13, 2 };
+int[] merge = array1.Concat(array2).ToArray(); // Concat() will combine both arrays
+Array.Sort(merge);
+Console.WriteLine(String.Join(", ", merge));
 /********************************/
 /**** Reverse integer digits ****/
 /********************************/
