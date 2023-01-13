692c73b00913750426cb45f2127085ec98ab94aa
Gabriel <gnicholauson@gmail.com>
2023-01-13 15:59:14 -0600

    find recurring numbers in an array

--- AlgorithmsAndDataStructure_Labs/Program.cs
+++ AlgorithmsAndDataStructure_Labs/Program.cs
@@ -10,3 +10,26 @@
  * 
 */
 
+/*********************************************/
+/**** Find recurring numbers in O(n) time ****/
+/*********************************************/
+int[] ints = new int[] { 1, 2, 3, 4, 7, 9, 2, 4 };
+string trackedNums = "";
+string recurringNums = "";
+
+foreach(int num in ints)
+{
+    if (!trackedNums.Contains(num.ToString()))
+    {
+        trackedNums += num.ToString() + ' ';
+    } else if (!recurringNums.Contains(num.ToString()))
+    {
+        recurringNums += num.ToString() + " ";
+    }
+}
+
+Console.WriteLine(recurringNums);
+
+/**************************************/
+/**** Combine arrays and sort them ****/
+/**************************************/
