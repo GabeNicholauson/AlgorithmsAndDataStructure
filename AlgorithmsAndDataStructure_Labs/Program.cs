47c784cdf1865f6c6277caab67310d4f19c9fb8b
Gabriel <gnicholauson@gmail.com>
2023-01-16 23:04:31 -0600

    Add header

--- AlgorithmsAndDataStructure_Labs/Program.cs
+++ AlgorithmsAndDataStructure_Labs/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿/*
+ * Lab 4
+ * Gabriel Nicholauson
+ * 
+ * This program will do 3 things:
+ * - Take a list of lists of ints and return a list with the highest number in each list.
+ * - Take a list of lists of ints and return the highest number out of all the lists,
+ *   the list and index it was found in.
+ * - orders a list of ints from lowest to highest.
+*/
