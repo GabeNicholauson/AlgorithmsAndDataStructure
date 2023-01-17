6148c5a966bee50c4a0d889c8fe49a13c5a9d5f6
Gabriel <gnicholauson@gmail.com>
2023-01-17 12:41:02 -0600

    add comments

--- AlgorithmsAndDataStructure_Labs/Program.cs
+++ AlgorithmsAndDataStructure_Labs/Program.cs
@@ -8,38 +8,41 @@
  *   the list and index it was found in.
  * - orders a list of ints from lowest to highest.
 */
-
-/**********************************/
-/**** Highest int in each list ****/
-/**********************************/
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
+/**********************************/
+/**** Highest int in each list ****/
+/**********************************/
+
+// The time complexity of this solution is O(n^2)
 List<List<int>> maxNumberInLists = new()
 {
     new List<int>(){ 1, 5, 3 },
     new List<int>(){ 9, 7, 3, -2 },
     new List<int>(){ 2, 1, 2 }
 };
-List<int?> allMaxNums = new ();
-int? max = null;
 
-foreach (List<int> l in maxNumberInLists)
+// The ? after int means that the int is allowed to be null
+List<int?> allMaxNums = new (); //tracks all the max nums
+int? max = null; //null incase there are some really low negative numbers
+
+foreach (List<int> l in maxNumberInLists) // goes through each list
 {
-    foreach (int num in l)
+    foreach (int num in l) // goes through each int in the current list
     {
         if (num > max && max != null)
         {
             max = num;
         }
-        else max ??= num;
+        else max ??= num; // The ??= operator checks if max is currently null and assigns num if it is
     }
     allMaxNums.Add(max);
     max = null;
 }
 
-for (int i = 0; i < allMaxNums.Count; i++)
+for (int i = 0; i < allMaxNums.Count; i++) // Prints the highest numbers in O(n) time
 {
     Console.WriteLine($"List {i + 1} has a maximum of {allMaxNums.ElementAt(i)}. ");
 }
@@ -48,24 +51,25 @@ for (int i = 0; i < allMaxNums.Count; i++)
 /**** Highest int out of all lists ****/
 /**************************************/
 
+// The time complexity of this solution is O(n^2)
 List<List<int>> grades = new()
 {
     new List<int>(){ 85,92, 67, 94, 94 },
     new List<int>(){ 50, 60, 57, 95 },
     new List<int>(){ 95 }
 };
-string location = "";
-int highestGrade = 0;
+string location = ""; // The index or indecis of the highest grade
+int highestGrade = 0; // tracks highest grade
 
-for (int i = 0; i < grades.Count; i++)
+for (int i = 0; i < grades.Count; i++) // goes through each list
 {
-    foreach (int g in grades.ElementAt(i))
+    foreach (int g in grades.ElementAt(i)) // goes through each int in the current list
     {
         if (g > highestGrade)
         {
             highestGrade = g;
             location = (i + 1).ToString();
-        } else if (g == highestGrade && location != (i + 1).ToString())
+        } else if (g == highestGrade && location != (i + 1).ToString()) // Checks if it's still in the same list
         {
             location += ", " + (i + 1).ToString(); 
         }
@@ -77,21 +81,22 @@ for (int i = 0; i < grades.Count; i++)
 
 Console.WriteLine($"The highest grade is {highestGrade}. This grade was found in class(es) {location}");
 
-
 /*******************************************/
 /**** Order list from least to greatest ****/
 /*******************************************/
+
+// The time complexity of this solution is O(n^2)
 List<int> orderThese = new() { 6, -2, 5, 3, 6, 3 ,8, 0 ,9, 76, 4, 1, -6, -7, 3 };
 
-for (int i = 1; i < orderThese.Count; i++)
+for (int i = 1; i < orderThese.Count; i++) // goes through each element in the list
 {
-    for (int j = i; j > 0; j--)
+    for (int j = i; j > 0; j--) // goes backwards through the list starting at the current element
     {
         if (orderThese.ElementAt(j) < orderThese.ElementAt(j - 1))
         {
-            int save = orderThese.ElementAt(j);
-            orderThese[j] = orderThese[j - 1];
-            orderThese[j - 1] = save;
+            int save = orderThese.ElementAt(j); // preserves element
+            orderThese[j] = orderThese[j - 1]; // moves an element up 1
+            orderThese[j - 1] = save; // replaces duplicate element with preserved element
         }
     }
 }
