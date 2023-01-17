0f1adef9277d488fdef91df76711af686614f116
Gabriel <gnicholauson@gmail.com>
2023-01-17 14:01:02 -0600

    fix question 1 to actually be in O(n) time

--- AlgorithmsAndDataStructure_Labs/Program.cs
+++ AlgorithmsAndDataStructure_Labs/Program.cs
@@ -13,22 +13,21 @@
 /*********************************************/
 /**** Find recurring numbers in O(n) time ****/
 /*********************************************/
-int[] ints = new int[] { 1, 2, 3, 4, 7, 9, 2, 4 };
-string trackedNums = "";
-string recurringNums = "";
+List<int> ints = new() { 13, 1, 2, 3, 4, 7, 9, 2, 4, 13 };
+HashSet<int> trackedNums = new HashSet<int>(); // Tracks unique nums
+string recurringNums = ""; // tracks the recurring nums
 
 foreach (int num in ints)
 {
-    if (!trackedNums.Contains(num.ToString()))
+    if (!trackedNums.Contains(num)) // Contains on a HashSet is O(1) time
     {
-        trackedNums += num.ToString() + ' ';
-    } else if (!recurringNums.Contains(num.ToString()))
+        trackedNums.Add(num);
+    } else
     {
-        recurringNums += num.ToString() + " ";
+        recurringNums += num + " ";
     }
 }
-
-Console.WriteLine(recurringNums);
+Console.WriteLine(recurringNums.Trim());
 
 /**************************************/
 /**** Combine arrays and sort them ****/
@@ -39,7 +38,7 @@ Console.WriteLine(recurringNums);
 int[] array1 = { 1, 2, 3, 4, 5, 5, 6, 7, 5, 6, 9, 4, 2 };
 int[] array2 = { 2, 5, 7, 9, 13, 9, 9, 13, 2 };
 int[] merge = array1.Concat(array2).ToArray(); // Concat() will combine both arrays
-Array.Sort(merge);
+Array.Sort(merge); // sorts in O(n log n) time
 Console.WriteLine(String.Join(", ", merge));
 /********************************/
 /**** Reverse integer digits ****/
